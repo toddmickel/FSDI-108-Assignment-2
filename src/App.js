c91e003bc7e2317e9764d44a72c92c2c766bc6c0
toddmickel <79764275+toddmickel@users.noreply.github.com>
2021-05-29 09:48:19 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import Footer from "./components/footer";
+import NavBar from "./components/navBar";
+import Catalog from "./components/catalog";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+
+      <Catalog></Catalog>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
